bab1c9a1fdd21d8e3b7c892d78692707a6fe2127
grover.v <grover.v@media.net>
2018-09-27 16:59:47 +0530

    Bugs Fixed And Database Added

--- background.js
+++ background.js
@@ -1,13 +1,15 @@
 
-
+var stopTracking = false;
 var startTime = 0;
 var focus = true;
+var idle = false;
 var socialSites = [ 'google', 'quora', 'youtube', 'facebook'];
 var Url = "default";
 var socialSiteTrack = {
 	name:null,
 	id:null
 };
+
 
 function isSiteTracked(siteUrl){
 	for(var elem of socialSites){
@@ -39,9 +41,68 @@ function isCurrentlyTracking(){
 	return false;
 }
 
-function storeActiveTimeOfSocialSite(siteName){
 
-}
+/******************************************************************
+						Structure Of Data Store
+TimesTamp(YYYYMMDD) - Object	{
+									Fb - [{st, end}, {st, end}]
+									.
+									.
+									google - same
+
+									Summary - {Fb : total, ___}
+								}
+
+
+
+******************************************************************/
+
+
+function storeActiveTimeOfSocialSite(stTime, endTime, curSite){
+	console.log(curSite);
+	var key = ""+stTime.getFullYear()+stTime.getMonth()+stTime.getDate();
+	var tmpEnd = null, tmpStart = null, val1 = null, val2 = null;
+
+	if(endTime.getDate() > stTime.getDate()){
+		tmpEnd = new Date(stTime.getFullYear(), stTime.getMonth(), stTime.getDate(), 23, 59);
+		tmpStart = new Date(endTime.getFullYear(), endTime.getMonth(), endTime.getDate());
+	}
+
+	if(tmpStart && tmpEnd){
+		val1 = {start : stTime.getTime(), end : tmpEnd.getTime()};
+		val2 = {start : tmpStart.getTime(), end : endTime.getTime()};
+	}
+	else
+		val1 = {start : stTime.getTime(), end : endTime.getTime()};
+
+	chrome.storage.sync.get([key], function(result){
+		console.log("Key created = " + key);
+		console.log("Result Fetched == " +  JSON.stringify(result));
+		var a = null;
+		if(result[key] === undefined){
+			console.log(key+" is not found in db");
+			a = {};
+			a["summary"] = {};
+			for(var site of socialSites){
+				a[site] = [];
+				a["summary"][site] = 0;
+			}
+		}
+		else
+			a = result[key];
+		a[curSite][a[curSite].length] = val1;
+		if(val2)
+			a[curSite][a[curSite].length] = val2;
+		a["summary"][curSite] += endTime - stTime;
+
+		chrome.storage.sync.set({[key]:a}, function(){
+			console.log("value saved = " + JSON.stringify(a)+ "Key = " + key);
+		});
+
+	});
+
+
+	}
 
 function getCurrentActiveTab(){
 
@@ -64,7 +125,7 @@ function calculateTimeSpent(siteUrl, tabId){
    			
   			console.log(timeSpent+ "  =  Total Time Spent on from cal time---" + siteName);
 
-   			storeActiveTimeOfSocialSite(siteName);
+   			storeActiveTimeOfSocialSite(startTime, new Date(), socialSiteTrack.name);
    			if(siteName)
    				startTime = new Date();
    			updateCurrentSite(siteName, tabId);
@@ -74,21 +135,21 @@ function calculateTimeSpent(siteUrl, tabId){
 
 
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	var siteUrl = changeInfo.url;
-	if(siteUrl){
-   		console.log("from Update -- " + siteUrl);
-   		calculateTimeSpent(siteUrl, tabId);
-	}
-});
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+// 	var siteUrl = changeInfo.url;
+// 	if(siteUrl){
+//    		console.log("from Update -- " + siteUrl);
+//    		calculateTimeSpent(siteUrl, tabId);
+// 	}
+// });
 
 
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-  chrome.tabs.get(activeInfo.tabId, function(tab){
-     console.log( "From tabs changed -- "+ tab.url);
-     calculateTimeSpent(tab.url, tab.id);
-  });
-}); 
+// chrome.tabs.onActivated.addListener(function(activeInfo) {
+//   chrome.tabs.get(activeInfo.tabId, function(tab){
+//      console.log( "From tabs changed -- "+ tab.url);
+//      calculateTimeSpent(tab.url, tab.id);
+//   });
+// }); 
 
 // chrome.tabs.onRemoved.addListener(function (tabId, removeInfo){
 // 	chrome.tabs.query({
@@ -133,37 +194,39 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
 // });
 
 
-chrome.idle.setDetectionInterval(15);
+// chrome.idle.setDetectionInterval(15);
 
-chrome.idle.onStateChanged.addListener(function(newState){
-	if(newState === "idle" || newState == "locked"){
-		//alert("You are idle");
-		if(isCurrentlyTracking() && focus){
-			var timeSpent = new Date() - startTime;
-   			console.log("Time Spent on closing site Idle close ---" + timeSpent);
-  			//alert(timeSpent+ "  =  Total Time Spent on idle close --- " + socialSiteTrack.name);
-  			updateCurrentSite(null,null);
-   			//storeActiveTimeOfSocialSite(siteName);
-		}
-		else
-			console.log("Focus is not there though you are sitting idle");
-	}
-	else if(focus){
-		//alert("You are active");
-		chrome.tabs.query({
-  			active: true,
-  			currentWindow: true
-		}, function(tabs) {
-  			var tab = tabs[0];
-  			var url = tab.url;
-  			console.log("You started doing work  --" + tab.url);
-  			var siteName = isSiteTracked(tab.url);
-  			if(siteName)
-  				startTime = new Date();
-  			updateCurrentSite(siteName,tab.id);
-		});
-	}
-});
+// chrome.idle.onStateChanged.addListener(function(newState){
+// 	if(newState === "idle" || newState == "locked"){
+// 		//alert("You are idle");
+// 		idle = true;
+// 		if(isCurrentlyTracking() && focus){
+// 			var timeSpent = new Date() - startTime;
+//    			console.log("Time Spent on closing site Idle close ---" + timeSpent);
+//   			//alert(timeSpent+ "  =  Total Time Spent on idle close --- " + socialSiteTrack.name);
+//   			updateCurrentSite(null,null);
+//    			storeActiveTimeOfSocialSite(startTime, new Date(), socialSiteTrack.name);
+// 		}
+// 		else
+// 			console.log("Focus or Trackable site is not there though you are sitting idle");
+// 	}
+// 	else if(focus){
+// 		idle = false;
+// 		//alert("You are active");
+// 		// chrome.tabs.query({
+//   // 			active: true,
+//   // 			currentWindow: true
+// 		// }, function(tabs) {
+//   // 			var tab = tabs[0];
+//   // 			var url = tab.url;
+//   // 			console.log("You started doing work  --" + tab.url);
+//   // 			var siteName = isSiteTracked(tab.url);
+//   // 			if(siteName)
+//   // 				startTime = new Date();
+//   // 			updateCurrentSite(siteName,tab.id);
+// 		// });
+// 	}
+// });
 
 // setInterval(chrome.windows.getCurrent({populate:true, windowTypes:["normal"]},function(w){
 			
@@ -173,22 +236,22 @@ chrome.idle.onStateChanged.addListener(function(newState){
 
 setInterval(() => {
 	chrome.windows.getCurrent({populate:true, windowTypes:["normal"]},function(w){
+		chrome.idle.queryState(15, function(newState){
 			
 				//console.log("Active Windows ----------"+w.id + " is focused ? - "+w.focused+" Window state " +w.state+ "Window Type = " + w.type);
-				if(!w.focused && focus){
-					focus =false;
+				if(!w.focused || newState != "active"){
 					console.log("Focus changed to off --" + focus);
 					if(isCurrentlyTracking()){
 							var timeSpent = new Date() - startTime;
    							//console.log("Time Spent on clos ---" + timeSpent);
   							console.log(timeSpent+ "  =  Total Time Spent before window closed --- " + socialSiteTrack.name);
+  							storeActiveTimeOfSocialSite(startTime, new Date(), socialSiteTrack.name);
   							updateCurrentSite(null,null);
   							//storeActiveTimeOfSocialSite(siteName);
   							
 					}
 				}
-				else if(!focus && w.focused){
-					focus = true;
+				else if(w.focused && newState === "active"){
 					console.log("Focus Changed back" + focus);
 					chrome.tabs.query({
   						active: true,
@@ -198,12 +261,40 @@ setInterval(() => {
   							var url = tab.url;
   							var siteName = isSiteTracked(tab.url);
   							console.log(" Started Time after window opened --- " + siteName);
-  							if(siteName)
-  								startTime = new Date();
-  							updateCurrentSite(siteName, tab.id);
+  							// if(siteName)
+  							// 	startTime = new Date();
+  							// updateCurrentSite(siteName, tab.id);
+  							calculateTimeSpent(tab.url, tab.id);
   							
 						});
-				}
-			
+				
+		
+			}
+			});
 		});
 }, 1000);
+
+// 		var value = [{start:4,end:5}];
+// 		var key = "2018805"
+//         chrome.storage.sync.set({[key] : value}, function() {
+//           console.log('Value is set to ' + JSON.stringify(value));
+//             chrome.storage.sync.get([key], function(result) {
+//           console.log('Value currently is ' + JSON.stringify(result[key]));
+//           // console.log(result[key][0].start)
+//           // console.log(3 > result[key][0].start && 3 > result[key][0].end);
+//           var val1 = {start:8, end:9};
+//           var val = result[key];
+//           val[val.length] = val1;
+//          chrome.storage.sync.set({[key] : val}, function() {
+//           console.log('Value is set to ' + JSON.stringify(val));
+//            chrome.storage.sync.get([key], function(result) {
+//           console.log('Value currently is ' + JSON.stringify(result[key]));
+//           console.log(result["1"] === undefined);
+//       });
+//       });
+//         });
+      
+//         });
+
+
+//chrome.storage.sync.clear(function(){console.log("clear all")});
