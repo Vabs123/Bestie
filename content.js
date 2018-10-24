20810158712e02dded4e106fb61096977f6e13cc
grover.v <grover.v@media.net>
2018-10-24 13:53:48 +0530

    Dynamic Time progress Added

--- content.js
+++ content.js
@@ -45,37 +45,54 @@ function showProgressBarAtTop(timeSpent, totalSocialTime){
     var per = getPercentage(timeSpent,totalSocialTime);
     var totalTime = getTimeString(""+getTimeSpend(totalSocialTime));
 
-    var myvar = '<div id="myprogressbar" style="z-index: 100000;background-color: white;width: 100%;POSITION: relative;padding: 0px;"><span id="close" style="float: right;position: relative;margin-right: 0.65%;vertical-align: middle; font-size: 150%; "><i class="fa fa-times fa-2x" aria-hidden="true"></i></span><div id="myProgress" style="width: 98%;background-color: #ddd;">  <div id="myBar" style="width: 50%; height: 25px; background-color: rgb(76, 175, 80); text-align: center; line-height: 25px; color: white; font-size: 20px;"></div><div id = "pBar" style="'+
+    var myvar = '<div id="myprogressbar" style="z-index: 100000;background-color: white;width: 100%;POSITION: relative;padding: 0px;"><span id="close" style="float: right;position: relative; margin:0%; padding: 0%; margin-right: 0.6%;cursor: pointer;vertical-align: middle; font-size: 24px; ">X</span><div id="myProgress" style="width: 98%;background-color: #ddd;">  <div id="myBar" style="width: 50%; height: 25px; background-color: rgb(76, 175, 80); text-align: center; line-height: 25px; color: white; font-size: 20px;"></div><div id = "pBar" style="'+
         '    position: absolute;'+
         '    z-index: 100000000;'+
         '    top: 2.5px;'+
         '    font-size: 18px;'+
         '    left: 40%;'+
-        '    color: white;'+
+        '    color: white; cursor: default;'+
         '    text-shadow: 2px 2px 8px black;'+
-        '">Hello</div></div></div>';
+        '">1m/2m</div></div></div>';
 
     var d = document.createElement('DIV');
     d.innerHTML = myvar;
     document.querySelector("HTML").prepend(d.firstChild);
     document.body.style.transform = "translateY(5px)";
+    document.querySelector("HTML").style.backgroundColor = "#FFF";
     document.getElementById("close").addEventListener("click",hideProgressBar);
     var progressBar = document.getElementById("myBar");
     var pBar = document.getElementById("pBar");
-    progressBar.style.width = "5%";
+    progressBar.style.width = per+"%";
     var timeUsed = getTimeString(""+getTimeSpend(timeSpent));
     pBar.innerHTML = "Time spent : "+timeUsed+"/"+totalTime+" ("+per+"%)";
-     // setInterval(() => {
-     //     if(isProgressBarAtTopPresent()){
-     //         timeSpent += 60000;
-     //         var per = getPercentage(timeSpent, totalSocialTime);
-     //         progressBar.style.width = per+"%";
-     //         timeUsed = getTimeString(""+getTimeSpend(timeSpent));
-     //         pBar.innerHTML = "Time spent : "+timeUsed+"/"+totalTime+" ("+per+"%)";
-     //     }
-     // }, 5000);
+
+    changeColor(per, progressBar);
+    // dynamicChange(timeSpent, totalSocialTime, totalTime, progressBar, pBar);
+     setInterval(() => {
+         if(isProgressBarAtTopPresent() && document.hasFocus() && (timeSpent < totalSocialTime)){
+             timeSpent += 60000;
+             if(timeSpent >= totalSocialTime)
+                 timeSpent = totalSocialTime;
+             var per = getPercentage(timeSpent, totalSocialTime);
+             changeColor(per, progressBar);
+             progressBar.style.width = per+"%";
+             timeUsed = getTimeString(""+getTimeSpend(timeSpent));
+             pBar.innerHTML = "Time spent : "+timeUsed+"/"+totalTime+" ("+per+"%)";
+         }
+     }, 60000);
 
 }
+
+function changeColor(per, progressBar){
+    if(per >= 80)
+        progressBar.style.backgroundColor = "Orange";
+    if(per >= 95)
+        progressBar.style.backgroundColor = "#d9534f";
+}
+
+
+
 
 function hideProgressBar(){
     document.getElementById("myprogressbar").style.display = "none";
